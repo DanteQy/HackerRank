4805e6de72e3e54a6efa91ef814473396c98edc4
DanteQy <dante.qyshka@outlook.it>
2018-03-04 17:39:13 +0100

    commit

---
+++ Algorithms/Warmup/Compare the Triplets/Solution.js
@@ -0,0 +1,53 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function solve(a0, a1, a2, b0, b1, b2){
+    var a = [a0, a1, a2];
+    var b = [b0, b1, b2];
+    var A = 0;
+    var B = 0;
+    for( i = 0; i < 3; i++){
+        if(a[i]>b[i]){
+            A++;
+        } else if(a[i]<b[i]) {
+            B++;
+        }
+    }
+    
+    return res = A+" "+B;
+}
+
+function main() {
+    var a0_temp = readLine().split(' ');
+    var a0 = parseInt(a0_temp[0]);
+    var a1 = parseInt(a0_temp[1]);
+    var a2 = parseInt(a0_temp[2]);
+    var b0_temp = readLine().split(' ');
+    var b0 = parseInt(b0_temp[0]);
+    var b1 = parseInt(b0_temp[1]);
+    var b2 = parseInt(b0_temp[2]);
+    var result = solve(a0, a1, a2, b0, b1, b2);
+    console.log(result);
+    
+
+
+}
